7d6d393b608fdc2fffd66523083d618ec08ee9a5
abdullhsharker <abdullahsharker99@gmail.com>
2022-03-20 17:50:22 +0600

    js file  added

---
+++ index.js
@@ -0,0 +1,27 @@
+// document.getElementById('deposit-btn').addEventListener('click', function () {
+//     const inputFild = document.getElementById('deposit-input');
+//     const inputFildValue = inputFild.value;
+//     const inputFildtext = parseFloat(inputFildValue);
+//     console.log(inputFildtext);
+
+//     const depositMoney = document.getElementById('depositMoney');
+//     const depositMonytext = depositMoney.innerText;
+//     const depositTotalmoney = parseFloat(depositMonytext);
+//     // console.log(depositTotalmoney);
+//     depositMoney.innerText = depositTotalmoney + inputFildtext;
+//     console.log(depositTotalmoney);
+//     inputFild.value = "";
+
+// })
+
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    // get input fild
+    const inputFild = document.getElementById('deposit-input');
+    const inputFildText = inputFild.value;
+    console.log(inputFildText);
+
+    // get deposite text
+
+
+
+})
